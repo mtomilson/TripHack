124fd0b43b169b11b210569af34b63998b2eaa97
mtomilson <mtomilson51@gmail.com>
2025-11-05 20:38:42 -0500

    feat(backend): initial commit for graphql setup

--- triphack/src/backend/server.ts
+++ triphack/src/backend/server.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
-import amadeus from "./routes/amadeus"
+import amadeus from "./routes/amadeus";
+
+
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api/amadeus", amadeus)
-
+app.use("/api/amadeus", amadeus);
 
 app.listen(5000, () => {
-    console.log("server running");
-})
+  console.log("server running");
+});
