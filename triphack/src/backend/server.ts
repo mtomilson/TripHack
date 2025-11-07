c2381cdef6d70bdbb77b60442769fc4a78c359bd
mtomilson <mtomilson51@gmail.com>
2025-11-06 23:03:54 -0500

    feat(backend): mounted graphql server

--- triphack/src/backend/server.ts
+++ triphack/src/backend/server.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import amadeus from "./routes/amadeus";
 import { expressMiddleware } from '@as-integrations/express4';
+import { typeDefs } from "./graphql/schemas/attractionsSchema";
+import { resolvers } from "./graphql/resolvers/attractionsResolver";
+import { ApolloServer } from "@apollo/server";
 
 
 const app = express();
@@ -9,6 +12,15 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/amadeus", amadeus);
 
+const server = new ApolloServer({typeDefs, resolvers});
+
+await server.start();
+
+app.use("/api/graphql", expressMiddleware(server));
+
+
 app.listen(5000, () => {
   console.log("server running");
+  console.log("GraphQL endpoint: http://localhost:5000/api/graphql");
+
 });
