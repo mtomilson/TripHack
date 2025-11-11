8f05d76ed2ce97e3f4da9d0459653bfeb1705057
mtomilson <mtomilson51@gmail.com>
2025-11-10 23:16:13 -0500

    feat(backend): rest api call to flights amadeus

--- triphack/src/backend/routes/amadeus.ts
+++ triphack/src/backend/routes/amadeus.ts
@@ -23,4 +23,32 @@ router.get("/search", async (req, res) => {
   }
 });
 
+router.get("/search/flights", async (req, res) => {
+  try {
+    const token = await getAccessToken();
+    const { origin, destination, departure, returnDate, roundTrip }= req.query;
+    if(origin === "" || destination === "" || departure === "") {
+      return res.status(400).json({error: "missing fields"});
+    }
+
+    const isRoundTrip = roundTrip === "true";
+
+    const url = `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${origin}&destinationLocationCode=${destination}&departureDate=${departure}&adults=1${isRoundTrip ? `&returnDate=${returnDate}` : ""}&currencyCode=USD`;
+    
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    })
+    
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+  }
+
+  
+})
+
 export default router;
